69922014169ed5a507ec83115689e939cadf6670
Crazy <1204506724@qq.com>
2020-03-25 22:12:11 +0800

    亮哥项目模版

---
+++ project-web/src/main/java/edu/nf/project/controller/AdminController.java
@@ -0,0 +1,17 @@
+package edu.nf.project.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Crazy
+ * @date 2020/3/25
+ */
+@RestController
+public class AdminController {
+    @GetMapping("/admin")
+    public  String say(){
+        return "您好第一次项目测试 请多多关照";
+    }
+}
